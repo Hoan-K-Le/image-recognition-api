f776c678d0f3ab2a975b3be4cf2cd7ded0723466
Hoan Le <hoanleecr@gmail.com>
2023-02-19 17:22:46 -0800

    finish layout and functionality

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,8 @@
 import React from 'react'
+// import Tilt from 'react-tilt';
 import Tilt from 'react-parallax-tilt'
-import './Logo.css'
 import brain from './brain.png'
+import './Logo.css'
 
 const Logo = () => {
   return (
@@ -9,10 +10,10 @@ const Logo = () => {
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
-        style={{ height: 150, width: 150, backgroundColor: 'darkgreen' }}
+        style={{ height: 150, width: 150 }}
       >
-        <div>
-          <img style={{ paddingTop: '5px' }} src={brain} alt="brain image" />
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingTop: '5px' }} alt="logo" src={brain} />
         </div>
       </Tilt>
     </div>
